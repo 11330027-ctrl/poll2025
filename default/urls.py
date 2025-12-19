86df0c4817a79e88d4888b947dc7479dcf664419
jimmy <11330027@dcsh.tp.edu.tw>
2025-12-19 11:58:46 +0800

    完成投票主題興曾修改刪除 選項新增修改刪除

--- default/urls.py
+++ default/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
-from .views import poll_list, PollList, PollView, pollvote
+from .views import poll_list, PollList, PollView, pollvote, PollCreate, PollEdit ,OptionCreate, OptionEdit, PollDelete, OptionDelete
 
 urlpatterns = [
     path("", poll_list),
     path("list", PollList.as_view(), name='poll_list'),
     path("<int:pk>/", PollView.as_view(), name='poll_view'),
-    path('<int:pk>/vote/', pollvote.as_view(), name='poll_vote'),
+    path('<int:oid>/vote/', pollvote.as_view(), name='poll_vote'),
+    path('add', PollCreate.as_view(), name='poll_create'),
+    path("<int:pk>/edit", PollEdit.as_view(), name="poll_edit"),
+    path("<int:pid>/add", OptionCreate.as_view(), name="option_create"),
+    path("<int:oid>/modify", OptionEdit.as_view(), name="option_edit"),
+    path("<int:pk>/delete)",PollDelete.as_view(), name='poll_delete'),
+    path("<int:pk>/remove", OptionDelete.as_view(), name='option_delete'),
 ]
